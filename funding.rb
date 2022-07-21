afca17932b063ebc8df48881410ff83dddf8be8d
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2022-07-21 11:20:39 -0600

    busy with other project

--- funding.rb
+++ funding.rb
@@ -1,9 +1,11 @@
+require_relative 'random'
 class Funding
     def initialize(project, funds = 0)
         @project = project
         @funds = funds
         @projects_last_balance = {"Help Kids": 1540, "Help small businesses": 12350, "Support athletes": 3490}
         @projects_goals = {"Help Kids": 10000, "Help small businesses": 100000, "Support athletes": 30000}
+        @rand = Random.new
     end
     def pledges
         @projects_last_balance.each do |project, funds|
@@ -18,7 +20,7 @@ class Funding
     end
     def style
 
-        puts "The #{@project} project has #{@funds}$ raised in this round and the current balance is #{@projects_last_balance[@project]}$ as of #{time} and the goal is #{@projects_goals[@project]}$ "
+        puts "The #{@project} project has #{@funds + (@rand.roll* 100)}$ raised in this round and the current balance is #{@projects_last_balance[@project]}$ as of #{time} and the goal is #{@projects_goals[@project]}$ "
     end
    
 end
